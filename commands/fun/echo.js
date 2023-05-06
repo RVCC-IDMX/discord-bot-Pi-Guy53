0e09fe0e7cd21957b9bf147c3a08d698b12fce33
UsrnGamesInc <71029215+Pi-Guy53@users.noreply.github.com>
2023-05-05 20:25:43 -0400

    added the echo command, had some fun with it

---
+++ commands/fun/echo.js
@@ -0,0 +1,40 @@
+const { SlashCommandBuilder, TextChannel } = require('discord.js');
+const user = require('../utility/user');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('echo')
+    .setDescription('Replies with your input!')
+    .addStringOption((option) => option.setName('input')
+      .setDescription('The input to echo back')
+      .setRequired(true)
+      .setMaxLength(25))
+
+    .addChannelOption((option) => option.setName('channel')
+      .setDescription('The channel to echo into'))
+
+    .addBooleanOption((option) => option.setName('ephemeral')
+      .setDescription('Whether or not the echo should be private')),
+
+  async execute(interaction) {
+    const userInput = { input: '', secretBool: 'false', channel: '' };
+    userInput.input = interaction.options.getString('input');
+
+    // console.log(interaction.options.getBoolean('ephemeral'));
+
+    if (interaction.options.getBoolean('ephemeral') != null) {
+      userInput.secretBool = interaction.options.getBoolean('ephemeral');
+    } else {
+      userInput.secretBool = false;
+    }
+
+    if (interaction.options.getChannel('channel') != null) {
+      // console.log(interaction.options.getChannel('channel'));
+      userInput.channel = interaction.options.getChannel('channel');
+      await interaction.guild.channels.cache.get(`${userInput.channel.id}`).send(userInput.input);
+      await interaction.reply({ content: `echoed to ${userInput.channel}`, ephemeral: userInput.secretBool });
+    } else {
+      await interaction.reply({ content: userInput.input, ephemeral: userInput.secretBool });
+    }
+  },
+};
